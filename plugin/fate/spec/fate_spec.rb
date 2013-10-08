a9b77ef5dfeaf948ee92cb57865ae935025fbbfe
Koga Osamu <osak.63@gmail.com>
2013-10-08 23:01:33 +0900

    Add spec

---
+++ plugin/fate/spec/fate_spec.rb
@@ -0,0 +1,33 @@
+require File.join(__dir__, "..", "fate.rb")
+
+describe ProjectF::Fate do
+  before(:each) do
+    @db = mock("fatedb")
+    @fate = ProjectF::Fate.new(@db)
+  end
+
+  context "autotweet" do
+    it "should call tweets_around" do
+      t = Time.now
+      #expect(@db).to receive(:tweets_around).with(t)
+      @db.should_receive(:tweets_around).with(t).and_return([])
+      @fate.autotweet(t)
+    end
+
+    it "should return tweet body" do
+      @db.should_receive(:tweets_around).and_return([text: "　#hashtag1 こんにちは #hashtag2"])
+      expect(@fate.autotweet(nil)).to eq("　■hashtag1 こんにちは ■hashtag2")
+    end
+
+    it "should not return mention" do
+      @db.should_receive(:tweets_around).and_return([text: "　#hashtag1 こんにちは @osa_k #hashtag2"])
+      expect(@fate.autotweet(nil)).to be_nil
+    end
+
+    it "should return nil on empty candidates" do
+      @db.should_receive(:tweets_around).and_return([])
+      expect(@fate.autotweet(nil)).to be_nil
+    end
+  end
+end
+
